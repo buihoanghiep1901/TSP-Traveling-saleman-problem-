d42ff683bd2435851a4647bb687eb1ba6c647963
dopedealer123 <bhoanghiep123456@gmail.com>
2022-07-08 23:50:07 +0700

    ok

---
+++ src/main/java/com/tsp/controller/graphView/vertexView.java
@@ -0,0 +1,54 @@
+package com.tsp.controller.graphView;
+
+import com.tsp.graph.Vertex;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
+
+public class vertexView  extends StackPane {
+
+    private Vertex v;
+    private final Label label = new Label();
+
+
+    public vertexView() {
+        this.setPrefSize(2, 14);
+        Circle circle = new Circle();
+        circle.setRadius(22);
+        circle.setFill(Color.DODGERBLUE);
+        circle.setStroke(Color.BLACK);
+        circle.setStrokeType(StrokeType.INSIDE);
+        this.getChildren().addAll(circle, label);
+    }
+
+    public vertexView(Vertex v) {
+        this.setPrefSize(2, 14);
+        this.v=v;
+        Circle circle = new Circle();
+        circle.setRadius(22);
+        circle.setFill(Color.DODGERBLUE);
+        circle.setStroke(Color.BLACK);
+        circle.setStrokeType(StrokeType.INSIDE);
+        label.setText(this.v.getId() + "");
+        this.getChildren().addAll(circle, label);
+    }
+
+    public Label getLabel() {
+        return this.label;
+    }
+
+    public void setLabel(int id) {
+        this.v.setId(Integer.toString(id));
+        this.label.setText(this.v.getId() + "");
+    }
+
+    public Vertex getV() {
+        return v;
+    }
+
+    public void setV(Vertex v) {
+        this.v = v;
+    }
+}
